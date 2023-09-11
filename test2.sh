46f3e33e266b46122e834f648c1f73f7903c9226
taka-chin <you@example.com>
2023-09-11 10:21:31 +0900

    test

---
+++ test2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+assert() {
+    printf '%-30s:' "\"$1\""
+
+    echo -n -e "$1" | bash >cmp >&-
+    expected=$?
+
+    echo -n -e "$1" | ./minishell >out >&-
+    actual=$?
+
+    diff cmp out >/dev/null && echo -n -e '  diff \033[0;32mOK\033[0;37m' || echo -n -e '  diff \033[0;31mNG\033[0;37m'
+
+    if [ "$actual" = "$expected" ]; then
+        echo -n -e '  status \033[0;32mOK\033[0;37m\n'
+    else
+        echo -n -e "  status \033[0;31mNG\033[0;37m, expected $expected but got $actual\n"
+    fi
+    echo 
+    rm out cmp
+}
+
+#$
+assert "ls"
