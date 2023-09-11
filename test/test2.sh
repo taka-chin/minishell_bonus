d44e72c9ba1d0362f78144b565fc876c9dfca78d
taka-chin <you@example.com>
2023-09-11 13:08:43 +0900

    diff status NG

--- test/test2.sh
+++ test/test2.sh
@@ -4,9 +4,11 @@ assert() {
     printf '%-30s:' "\"$1\""
 
     echo -n -e "$1" | bash >cmp | exec >&-
+    echo -n -e "$1" | bash
     expected=$?
 
-    echo -n -e "$1" | ../minishell >out | exec >&-
+    echo -n -e "$1" | ./minishell >out | exec >&-
+    echo -n -e "$1" | ./minishell
     actual=$?
 
     diff cmp out >/dev/null && echo -n -e '  diff \033[0;32mOK\033[0;37m' || echo -n -e '  diff \033[0;31mNG\033[0;37m'
@@ -17,7 +19,7 @@ assert() {
         echo -n -e "  status \033[0;31mNG\033[0;37m, expected $expected but got $actual\n"
     fi
     echo 
-    # rm out cmp
+    rm out cmp
 }
 
 #$
